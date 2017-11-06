251dd7d75b960ea4571dd8083c280f13aef1959b
karitra <karev.alexandre@gmail.com>
2017-11-06 23:34:06 +0300

    feat: complete orcas pod update

--- src/engine.rs
+++ src/engine.rs
@@ -16,14 +16,13 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::sync::{Arc, RwLock};
 use std::str::FromStr;
-use std::time;
+use std::time::{self, UNIX_EPOCH};
 use std::iter::Iterator;
 use std::net;
 use std::collections::HashMap;
 
 use secure::make_ticket_service;
 use errors::CombinedError;
-use types::SubscribeMessage;
 use config::Config;
 use resources::{Endpoint, NodeInfo};
 
@@ -35,23 +34,34 @@ use unicorn::{
 
 use orca;
 use orca::{
+    OrcaRecord,
     SyncedOrcasPod,
     DEFAULT_WEB_PORT
 };
 
 
-const GATHER_INTERVAL_SEC: u64 = 2;
+const ONE_HOUR_IN_SECS: u64 = 1 * 60 * 60;
+const GATHER_INTERVAL_SEC: u64 = 10;
 
+
+pub type SubscribeMessage = (i64, Vec<String>);
 
 pub type Cluster = HashMap<String, NodeInfo>;
 pub type SyncedCluster = RwLock<Cluster>;
+
 type AuthHeaders = Vec<RawHeader>;
 type UuidNodeInfo = (String, Option<NodeInfo>);
 
 
+#[derive(Debug, Clone)]
+pub struct NetInfo {
+    hostname: String,
+    endpoints: Vec<Endpoint>
+}
+
 trait ClusterInterface {
     fn update(&mut self, nodes: Vec<UuidNodeInfo>);
-    fn endpoints(&self) -> HashMap<String, Vec<Endpoint>>;
+    fn hosts(&self) -> HashMap<String, NetInfo>;
 }
 
 impl ClusterInterface for Cluster {
@@ -67,10 +77,14 @@ impl ClusterInterface for Cluster {
         } // for
     }
 
-    fn endpoints(&self) -> HashMap<String, Vec<Endpoint>> {
-        let mut endpoints: HashMap<String, Vec<Endpoint>> = HashMap::with_capacity(self.len());
+    fn hosts(&self) -> HashMap<String, NetInfo> {
+        let mut endpoints: HashMap<String, NetInfo> = HashMap::with_capacity(self.len());
         for (uuid, node_info) in self {
-            endpoints.entry(uuid.clone()).or_insert(node_info.endpoints.clone());
+            let net = NetInfo {
+                hostname: node_info.hostname.clone(),
+                endpoints: node_info.endpoints.clone()
+            };
+            endpoints.entry(uuid.clone()).or_insert(net);
         }
         endpoints
     }
@@ -82,7 +96,6 @@ fn make_auth_headers(header: Option<String>) -> Option<AuthHeaders>  {
         Some(vec![ RawHeader::new("authorization".as_bytes(), hdr.into_bytes()) ])
     )
 }
-
 
 pub fn subscription<'a>(handle: Handle, config: &Config, path: &'a str, cluster: Arc<SyncedCluster>)
     -> Box<Future<Item=(), Error=CombinedError> + 'a>
@@ -113,10 +126,6 @@ pub fn subscription<'a>(handle: Handle, config: &Config, path: &'a str, cluster:
 
     let nodes_future = rx.for_each(move |(version, nodes)| {
         println!("got from queue {} item(s) with version {}", nodes.len(), version);
-
-        // for node in &nodes {
-        //     println!("\t{} {:?}", path, node);
-        // }
 
         let proxy = Rc::clone(&proxy);
         let cluster = Arc::clone(&cluster);
@@ -161,28 +170,32 @@ pub fn subscription<'a>(handle: Handle, config: &Config, path: &'a str, cluster:
     Box::new(subscibe_future)
 }
 
-fn make_requests<C>(cli: Rc<hyper::Client<C>>, uuid: String, ep: Endpoint)
-    -> Box<Future<Item=(), Error=CombinedError>>
+type OrcaRequestResult = (/* hostname: */ String, orca::Orca);
+
+fn make_requests<'a, C>(
+    client: &'a hyper::client::Client<C>, endpoint: Endpoint, net_info: &NetInfo)
+    -> Box<Future<Item=OrcaRequestResult, Error=CombinedError> + 'a>
 where
-    C: hyper::client::Connect
+    C: hyper::client::Connect + 'a
 {
     fn ip6_uri_from_string(uri: &str, port: u16, path: &str)
         -> Result<hyper::Uri, hyper::error::UriError>
     {
         let uri = format!("{}://[{}]:{}/{}", orca::DEFAULT_WEB_SCHEME, uri, port, path);
-        println!("uri: {:?}", uri);
         uri.parse::<hyper::Uri>()
     }
 
-    fn get<'a,T,C>(cli: &hyper::Client<C>, uri: hyper::Uri)
+    // TODO: make connector pluggable
+    fn get<'a,C,T>(cli: &'a hyper::Client<C>, uri: hyper::Uri)
         -> Box<Future<Item=T, Error=CombinedError> + 'a>
     where
-        C: hyper::client::Connect,
+        C: hyper::client::Connect + 'a,
         T: serde::de::DeserializeOwned + 'a
     {
-        println!("get for {:?}", uri);
+        // println!("get for {:?}", uri);
         let data = cli.get(uri)
             .and_then(|res| {
+                // println!("reusult {}", res.status());
                 res.body().fold(Vec::new(), |mut acc, chunk| {
                     acc.extend(&chunk[..]);
                     future::ok::<Vec<u8>,hyper::Error>(acc)
@@ -199,64 +212,59 @@ where
         Box::new(data)
     }
 
-    let info_uri = ip6_uri_from_string(&ep.host_str(), DEFAULT_WEB_PORT, "info");
-    let state_uri = ip6_uri_from_string(&ep.host_str(), DEFAULT_WEB_PORT, "state");
-    let _metrics_uri = ip6_uri_from_string(&ep.host_str(), DEFAULT_WEB_PORT, "metrics");
+    let info_uri = ip6_uri_from_string(&endpoint.host_str(), DEFAULT_WEB_PORT, "info");
+    let state_uri = ip6_uri_from_string(&endpoint.host_str(), DEFAULT_WEB_PORT, "state");
+    let _metrics_uri = ip6_uri_from_string(&endpoint.host_str(), DEFAULT_WEB_PORT, "metrics");
 
-    let _uuid = Rc::new(uuid);
-
-    let cli_for_info = Rc::clone(&cli);
     let info_future = future::result(info_uri)
-        .and_then(|uri| {
-            println!("ready to connect to uri {:?}", uri);
-            Ok(uri)
-        })
         .map_err(CombinedError::UriParseError)
         .and_then(|uri| { // TODO: Debug clusure, remove
-            println!("making info request for {:?}", uri);
+            // println!("making info request for {:?}", uri);
             Ok(uri)
         })
-        .and_then(move |uri| get::<orca::Info, C>(&cli_for_info, uri))
-        .and_then(|info| {
-            println!("info {:?}", info);
-            Ok(())
-        });
+        .and_then(move |uri| get::<C, orca::Info>(client, uri));
 
-    let cli_for_state = Rc::clone(&cli);
     let state_future = future::result(state_uri)
         .map_err(CombinedError::UriParseError)
         .and_then(|uri| { // TODO: Debug clusure, remove
-            println!("making state request {:?}", uri);
+            // println!("making state request {:?}", uri);
             Ok(uri)
         })
-        .and_then(move |uri| get::<orca::CommitedState, C>(&cli_for_state, uri))
-        .and_then(|state| {
-            println!("state {:?}", state);
-            Ok(())
+        .and_then(move |uri| get::<C, orca::CommitedState>(client, uri));
+
+    let hostname = net_info.hostname.clone();
+    let endpoint = endpoint.clone();
+
+    let request_result = info_future.join(state_future)
+        .then(move |r| match r {
+            Ok((info, state)) => {
+                let orca = orca::Orca {
+                    endpoints: vec![ endpoint ],
+                    state,
+                    info,
+                };
+                Ok((hostname, orca))
+            },
+            Err(e) => Err(e)
         });
 
-    //
-    // let r = Box::new(state_future.join(info_future).then(|r|
-    //     match r {
-    //         Ok(v) => {println!("it seems make request done {:?}", v); future::ok(())},
-    //         Err(e) => {println!("error {:?}", e); future::err(e)}
-    //     }
-    // ));
-    //
-    // println!("result is {:?}", r);
-    //
-
-    Box::new(state_future.join(info_future).then(|r| {
-        println!("request completion {:?}", r);
-        Ok(())
-    }))
+    Box::new(request_result)
 }
 
 
+#[allow(dead_code)]
+fn dump_cls(cls: &Cluster) {
+    for (uuid, node) in cls {
+        println!("{} {}", uuid, node.hostname);
+    }
+}
+
 // TODO: current implemetation will cancel request in case of error in any one,
 //       reimplement to make complete other in case of some tasks fail.
-pub fn gather(handle: Handle, cluster: Arc<SyncedCluster>, _orcas: Arc<SyncedOrcasPod>)
- -> Box<Future<Item=(), Error=CombinedError>>
+pub fn gather<'a,C>(client: &'a hyper::client::Client<C>, cluster: Arc<SyncedCluster>, orcas: Arc<SyncedOrcasPod>)
+    -> Box<Future<Item=(), Error=CombinedError> + 'a>
+where
+    C: hyper::client::Connect + 'a
 {
 
     fn is_ipv6(addr: &Endpoint) -> bool {
@@ -266,44 +274,73 @@ pub fn gather(handle: Handle, cluster: Arc<SyncedCluster>, _orcas: Arc<SyncedOrc
         }
     }
 
+    let hosts = cluster.read().unwrap().hosts();
     let mut gather_strides = Vec::with_capacity(cluster.read().unwrap().len());
-    let endpoints = cluster.read().unwrap().endpoints();
 
-    println!("cluster size {}", endpoints.len());
+    println!("cluster size {}", hosts.len());
 
-    let cli_handle = handle.clone();
-    let cli = Rc::new(hyper::Client::new(&cli_handle));
+    for (num, (uuid, net)) in hosts.iter().enumerate() {
 
-    let handle = Rc::new(handle);
+        let to_sleep = time::Duration::new(num as u64 % GATHER_INTERVAL_SEC, 0);
+        println!("timeout {:?}", (num as u64 % GATHER_INTERVAL_SEC));
 
-    for (num, (uuid, ep)) in endpoints.iter().enumerate() {
-        let to_sleep = time::Duration::new((num as u64 % GATHER_INTERVAL_SEC), 0);
+        let gather_bootstrap = match Timeout::new(to_sleep, &client.handle()) {
+            Ok(_timout) => {
+                let eps_v6: Vec<_> = net.endpoints.iter().filter(|net| is_ipv6(net)).collect();
 
-        let cli = Rc::clone(&cli);
-        let gather_bootstrap = Timeout::new(to_sleep, &handle)
-            .map_err(CombinedError::IOError)
-            .and_then(move |_| Ok((uuid, ep)))
-            .and_then(|(uuid, eps)| {
-                let eps_v6: Vec<_> = eps.iter().filter(|ep| is_ipv6(ep)).collect();
+                println!("uuid {}", uuid);
 
                 //
-                // TODO: first address taken (if any), but should we peek random one
+                // TODO: first address taken (if any), but should we peek a random one?
                 //
                 match eps_v6.first() {
-                    Some(ref ep) => {
-                        let requests = make_requests(cli, uuid.to_string(), (**ep).clone());
-                        println!("preparing request for {}: {:?}", uuid, ep);
-                        Ok(requests)
-                    },
+                    Some(ref ep) => make_requests(client, (**ep).clone(), net),
                     None => {
-                        let error_message = format!("can't find ip6 address for uuid {} within hosts {:?}", uuid, eps);
-                        Err(CombinedError::Other(error_message))
+                        let error_message = format!("can't find ip6 address for uuid {} within host {:?}", uuid, net);
+                        Box::new(future::err(CombinedError::Other(error_message)))
                     }
                 }
+            },
+
+            // Propogate timeout error as a future.
+            Err(e) => Box::new(future::err(CombinedError::IOError(e)))
+        };
+
+        let completion = gather_bootstrap
+            .then(|r| match r {
+                Ok(r) => Ok(Some(r)),
+                // TODO: For now error is ignored silently, but we should
+                //       report it to some kind of logger someday.
+                Err(e) => { println!("error on request {:?}", e); Ok(None) }
             });
 
-        gather_strides.push(gather_bootstrap);
+        gather_strides.push(completion);
     }
 
-    Box::new(future::join_all(gather_strides).and_then(|_| Ok(())))
+    let result = future::join_all(gather_strides)
+        .and_then(move |responses| {
+
+            let now = time::SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+            let span = time::Duration::from_secs(ONE_HOUR_IN_SECS);
+
+            { // Remove old records.
+                let mut orcas = orcas.write().unwrap();
+                orcas.retain(|_host, orca|
+                    now - time::Duration::from_secs(orca.update_timestamp) < span);
+            }
+
+            {
+                let mut wr_orcas = orcas.write().unwrap();
+                for val in responses {
+                    if let Some((host, orca)) = val {
+                        let record = OrcaRecord { orca, update_timestamp: now.as_secs() };
+                        wr_orcas.insert(host, record);
+                    }
+                }
+            }
+
+            Ok(())
+        });
+
+    Box::new(result)
 }
