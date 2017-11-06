46717abbfe9ce08249684471292b1fa310ef2ad3
karitra <karev.alexandre@gmail.com>
2017-11-06 23:22:34 +0300

    refact(orca.rs): pub fields + wrap orca into metainfo struct

--- src/orca.rs
+++ src/orca.rs
@@ -7,9 +7,10 @@ use resources::Endpoint;
 
 pub const DEFAULT_WEB_SCHEME: &str = "http";
 pub const DEFAULT_WEB_PORT: u16 = 8877;
-pub const DEFAULT_STATUS_PORT: u16 = 9878;
+#[allow(dead_code)] pub const DEFAULT_STATUS_PORT: u16 = 9878;
 
 
+#[allow(dead_code)]
 pub enum WebHandler {
     State,
     Info,
@@ -27,23 +28,25 @@ pub struct AppState {
 
 // mapping: app -> state
 pub type CommitedState = HashMap<String, AppState>;
-// mapping: uuid -> Orca struct
-pub type OrcasPod = HashMap<String, Orca>;
+// mapping: hostname -> Orca struct
+pub type OrcasPod = HashMap<String, OrcaRecord>;
 pub type SyncedOrcasPod = RwLock<OrcasPod>;
 
 
 #[derive(Debug, Deserialize)]
 pub struct Info {
-    uptime: i64,
-    version: String,
-    uuid: String, // unused as we know uuid in advance
+    pub uptime: i64,
+    pub version: String,
+    pub uuid: String,
 }
 
 pub struct Orca {
-    // Note: duplicated from cluster
-    hostname: String,
-    endpoints: Vec<Endpoint>,
+    pub endpoints: Vec<Endpoint>,
+    pub state: CommitedState,
+    pub info: Info,
+}
 
-    state: CommitedState,
-    info: Info,
+pub struct OrcaRecord {
+    pub orca: Orca,
+    pub update_timestamp: u64,
 }
